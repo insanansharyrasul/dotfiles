1c69b23ae8f7abc24d1fd20bb7945b1b8ecedd54
insanansharyrasul <insanansryrasul21@gmail.com>
2025-02-12 19:34:25 +0700

    new - added fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,9 @@
+oh-my-posh init fish --config $HOME/.poshthemes/montys.omp.json | source
+alias vim nvim
+alias vi nvim
+
+# if status is-interactive
+    # Commands to run in interactive sessions can go here
+# end
+
+set fish_greeting
