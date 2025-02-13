23b5081dbe24948d29584721a12fd361cf0ca69a
insanansharyrasul <insanansryrasul21@gmail.com>
2025-02-13 11:35:38 +0700

    new - added neovim

---
+++ .config/nvim/init.lua
@@ -0,0 +1,24 @@
+-- Basic Vim Setup
+vim.opt.relativenumber = true
+vim.opt.nu = true
+vim.api.nvim_set_hl(0, 'NonText', { fg = 'bg' })
+vim.opt.timeoutlen = 1000
+vim.opt.ttimeoutlen = 0
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.incsearch = true
+vim.opt.hlsearch = false
+vim.g.mapleader = " "
+
+-- Plugins
+local vim = vim
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin')
+
+Plug('m4xshen/autoclose.nvim')
+Plug('https://tpope.io/vim/surround.git')
+
+vim.call('plug#end')
+
+require("autoclose").setup()
