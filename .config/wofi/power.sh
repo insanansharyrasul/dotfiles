c8f9f9bb492956213ffa1e6394dd4e9d9617659f
insanansharyrasul <insanansryrasul21@gmail.com>
2025-05-18 19:21:08 +0700

    update - add configuration files for Sway, Waybar, Wofi, and Catppuccin theme

---
+++ .config/wofi/power.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# filepath: ~/.config/wofi/power.sh
+
+entries="⏻ Shutdown\n⟳ Reboot\n⏾ Suspend\n⏏ Logout\n🔒 Lock"
+
+selected=$(echo -e $entries | wofi --dmenu --cache-file /dev/null --insensitive --width 250 --height 210 --style ~/.config/wofi/power_style.css --prompt "Power Menu" | awk '{print tolower($2)}')
+
+case $selected in
+  shutdown)
+    exec systemctl poweroff
+    ;;
+  reboot)
+    exec systemctl reboot
+    ;;
+  suspend)
+    exec systemctl suspend
+    ;;
+  logout)
+    exec loginctl terminate-session ${XDG_SESSION_ID-}
+    ;;
+  lock)
+    exec swaylock -f -c 000000
+    ;;
+esac
