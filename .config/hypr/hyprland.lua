9ebf6ed4d3ea93cbcafdffba201908b174bc6251
insanansharyrasul <insanansryrasul21@gmail.com>
2026-05-16 09:49:42 +0700

    feat: manual scss and yuck waybar change

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -50,9 +50,9 @@ end
 hl.workspace_rule({ workspace = "1", monitor = "eDP-1",    persistent = true })
 hl.workspace_rule({ workspace = "2", monitor = "eDP-1",    persistent = true })
 hl.workspace_rule({ workspace = "3", monitor = "eDP-1",    persistent = true })
-hl.workspace_rule({ workspace = "4", monitor = "HDMI-A-1", persistent = true })
-hl.workspace_rule({ workspace = "5", monitor = "HDMI-A-1", persistent = true })
-hl.workspace_rule({ workspace = "6", monitor = "HDMI-A-1", persistent = true })
+-- hl.workspace_rule({ workspace = "4", monitor = "HDMI-A-1", persistent = true })
+-- hl.workspace_rule({ workspace = "5", monitor = "HDMI-A-1", persistent = true })
+-- hl.workspace_rule({ workspace = "6", monitor = "HDMI-A-1", persistent = true })
 
 
 --------------------------
